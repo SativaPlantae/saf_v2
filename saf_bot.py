03494530a9caac3a379bb890df12eb1667524780
SativaPlantae <plantaesativa23@gmail.com>
2025-04-20 18:11:52 -0300

    Update saf_bot.py

--- saf_bot.py
+++ saf_bot.py
@@ -10,7 +10,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
-from langchain.schema import Document  # ✅ para langchain 0.0.320
+from langchain.schema import Document
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
